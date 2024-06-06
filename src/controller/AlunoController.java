036399313cc66d82a7e30c26ba92d5372f5753d2
guilhermesouza23 <tiago1287souza@gmail.com>
2024-06-05 21:25:16 -0300

    Conclusao

--- src/controller/AlunoController.java
+++ src/controller/AlunoController.java
@@ -16,30 +16,61 @@ public class AlunoController {
     public void Controller() {
 
         System.out.println("Digite sua idade:");
-        
-        
+
         Scanner scanner = new Scanner(System.in);
+
         
-        System.out.println("Digite sua nome:");
-        Scanner sc = new Scanner(System.in);
-       
 
         Aluno aluno = new Aluno();
 
         aluno.setIdade(scanner.nextInt());
+        
+        System.out.println("Digite sua nome:");
+        Scanner sc = new Scanner(System.in);
         aluno.setNome(sc.next());
+        
+        
+        
+        System.out.println("Digite sua nota1:");
+        aluno.setNota1(scanner.nextDouble());
+        System.out.println("Digite sua nota2:");
+        aluno.setNota2(scanner.nextDouble());
+        System.out.println("Digite sua nota3:");
+        aluno.setNota3(scanner.nextDouble());
+        System.out.println("Digite sua nota4:");
+        aluno.setNota4(scanner.nextDouble());
+        
+        
+        double resultado = (aluno.getNota1()+aluno.getNota2()+aluno.getNota3()+aluno.getNota4()) / 4;
+        
+        
 
         if (aluno.getIdade() >= 18 && aluno.getIdade() < 64) {
 
-            System.out.println("VOCE PODE ESUDAR:  " +aluno.getNome());
+            System.out.println("VOCE PODE ESUDAR:  " + aluno.getNome());
             
+
         } else {
             if (aluno.getIdade() < 18) {
-                System.out.println("NAO PODE");
+                System.out.println("NAO PODE" + aluno.getNome());
             } else {
                 if (aluno.getIdade() > 64) {
 
-                    System.out.println("IDOSO");
+                    System.out.println("IDOSO" + aluno.getNome());
+                }
+            }
+        }
+        
+        if(resultado < 3) {
+            
+            System.out.println("Reprovado");
+            
+        } else {
+            if (resultado < 6) {
+                System.out.println("FINAL");
+            } else {
+                if (resultado >= 6) {
+                    System.out.println("APROVADO");
                 }
             }
         }
